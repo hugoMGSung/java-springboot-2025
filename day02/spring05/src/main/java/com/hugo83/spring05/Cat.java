f6760787a99ec044112ec72171c7b0d15ff52b18
myplc <plctohmi@gmail.com>
2025-04-03 17:29:57 +0900

    3일차 커밋

--- day02/spring05/src/main/java/com/hugo83/spring05/Cat.java
+++ day02/spring05/src/main/java/com/hugo83/spring05/Cat.java
@@ -2,6 +2,6 @@ package com.hugo83.spring05;
 
 public class Cat extends Animal {
     void cry() {
-        System.out.println(super.name + "이(가) 웁니다. 야옹!!");
+        System.out.println(super.getName() + "이(가) 웁니다. 야옹!!");
     }
 }
